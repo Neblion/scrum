d8fe14c73ac823ee2f1221218eaa52796859e490
Thomas BIBARD <thomas.bibard@neblion.net>
2012-10-02 11:41:57 +0200

    composer.json: add post-install-script copy parameters.yml.dist to parameters.yml

---
+++ src/Neblion/ScrumBundle/Composer/ScriptHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Neblion\ScrumBundle\Composer;
+
+use Symfony\Component\ClassLoader\ClassCollectionLoader;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+class ScriptHandler
+{
+    public static function InstallParameters($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not install the requirements file.'.PHP_EOL;
+
+            return;
+        }
+
+        copy($appDir.'/config/parametes.yml.dist', $appDir.'/config/parameters.yml');
+    }
+
+    protected static function getOptions($event)
+    {
+        $options = array_merge(array(
+            'symfony-app-dir' => 'app',
+            'symfony-web-dir' => 'web',
+            'symfony-assets-install' => 'hard'
+        ), $event->getComposer()->getPackage()->getExtra());
+
+        $options['symfony-assets-install'] = getenv('SYMFONY_ASSETS_INSTALL') ?: $options['symfony-assets-install'];
+
+        $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
+
+        return $options;
+    }
+
+    protected static function getPhp()
+    {
+        $phpFinder = new PhpExecutableFinder;
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
+    }
+}
