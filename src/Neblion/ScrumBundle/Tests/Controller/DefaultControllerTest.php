c4da2addae40ffa5e946ed1353cb44b4f2d65ae5
Thomas BIBARD <thomas.bibard@neblion.net>
2012-10-08 09:26:58 +0200

    Add tests for test...

--- src/Neblion/ScrumBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Neblion/ScrumBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,10 +9,27 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-        $this->assertTrue($crawler->filter('html:contains("Dashboard")')->count() == 0);
-        $this->assertTrue($crawler->filter('html:contains("Hello, world!")')->count() == 1);
+        $crawler = $client->request('GET', '/login');
+        //$this->assertTrue($crawler->filter('html:contains("Dashboard")')->count() == 0);
+        //$this->assertTrue($crawler->filter('html:contains("Hello, world!")')->count() == 1);
+        
+        $form = $crawler->selectButton('Login')->form();
+        $client->submit($form, array(
+            '_username' => 'admin',
+            '_password'  => 'test'
+        ));
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $crawler = $client->followRedirect();
+        //var_dump($client->getResponse()->getContent());
     }
     
-    
+    public function testAuth()
+    {
+        /*
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        var_dump($client->getResponse()->getContent());
+        $this->assertTrue($crawler->filter('html:contains("Your projects")')->count() == 2);
+        */
+    }
 }
