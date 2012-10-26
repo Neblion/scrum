b9413d3884afb67dc5b9aed2ad8bfe40dca53b28
Thomas BIBARD <thomas.bibard@neblion.net>
2012-10-26 14:10:41 +0200

    Team add functional tests

---
+++ src/Neblion/ScrumBundle/Tests/Controller/TeamControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Neblion\ScrumBundle\Tests\Controller;
+
+use Neblion\ScrumBundle\Tests\WebTestCase;
+
+class TeamControllerTest extends WebTestCase
+{
+    public function testShow()
+    {
+        $client = $this->login('admin', 'test');
+        $crawler = $client->request('GET', '/team/1/show');
+        // Test html template
+        // Title of main widget-box
+        $this->assertTrue($crawler->filter('html:contains("Team members")')->count() == 1);
+        // 5 tr => 5 members for project->id = 1
+        $this->assertEquals(5, $crawler->filter('table tbody tr')->count());
+    }
+}
