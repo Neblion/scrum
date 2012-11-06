faca2f18c1c6e68cd2af93d11186620633fa0d14
Thomas BIBARD <thomas.bibard@neblion.net>
2012-11-06 02:45:33 +0100

    Add DataFixtures for installation

---
+++ src/Neblion/ScrumBundle/DataFixtures/Install/LoadProcessStatusData.php
@@ -0,0 +1,51 @@
+<?php
+namespace Neblion\ScrumBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+use Neblion\ScrumBundle\Entity\ProcessStatus;
+
+class LoadProcessStatusData extends AbstractFixture implements OrderedFixtureInterface
+{
+    private $manager; 
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        
+        $entities = array(
+            array('name' => 'To Do'),
+            array('name' => 'In Progress'),
+            array('name' => 'Done'),
+        );
+     
+        foreach ($entities as $entity) {
+            $this->newEntity($entity);
+        }
+        
+        $this->manager->flush();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 2; 
+    }
+    
+    private function newEntity($params)
+    {
+        $status = new ProcessStatus();
+        $status->setName($params['name']);
+        $this->manager->persist($status);
+    }
+    
+    
+}
