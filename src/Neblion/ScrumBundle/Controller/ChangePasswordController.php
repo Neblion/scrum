dfd50a51d56ee041ade1a5de0ddd1df17a3b6e12
Thomas BIBARD <thomas.bibard@neblion.net>
2012-11-08 08:42:34 +0100

    Add password modification in profile edition

--- src/Neblion/ScrumBundle/Controller/ChangePasswordController.php
+++ src/Neblion/ScrumBundle/Controller/ChangePasswordController.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the FOSUserBundle package.
- *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Neblion\ScrumBundle\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -23,29 +14,24 @@ use FOS\UserBundle\Model\UserInterface;
 class ChangePasswordController extends BaseController
 {
     /**
-     * Change user password
+     * Generate the redirection url when the resetting is completed.
+     *
+     * @param \FOS\UserBundle\Model\UserInterface $user
+     *
+     * @return string
      */
-    public function changePasswordAction()
+    protected function getRedirectionUrl(UserInterface $user)
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-
-        $form = $this->container->get('fos_user.change_password.form');
-        $formHandler = $this->container->get('fos_user.change_password.form.handler');
-
-        $process = $formHandler->process($user);
-        if ($process) {
-            $this->container->get('session')->setFlash('success', 'The password has been changed with success !');
-            return new RedirectResponse($this->container->get('router')->generate('neblion_scrum_welcome'));
-        }
-
-        return $this->container->get('templating')->renderResponse(
-            'FOSUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
-            array('form' => $form->createView())
-        );
+        return $this->container->get('router')->generate('profile_edit');
     }
 
+    /**
+     * @param string $action
+     * @param string $value
+     */
+    protected function setFlash($action, $value)
+    {
+        $this->container->get('session')->setFlash('success', 'The password was updated with success !');
+    }
     
 }
